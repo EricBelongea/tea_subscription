b599551f6e22c90d6a3218c32d240600fab376e6
Eric Belongea <eric.belongea@gmail.com>
2024-01-25 12:11:27 -0700

    Subscription creation happy path testing

--- spec/requests/api/v0/subscriptions_spec.rb
+++ spec/requests/api/v0/subscriptions_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe "Subscriptions Requests" do
 
   end
   it "Subscribe a Customer to a tea Subscription" do
-    require 'pry'; binding.pry
-
     params = {
       title: "My First Tea Order",
       status: true,
@@ -21,7 +19,22 @@ RSpec.describe "Subscriptions Requests" do
 
     rb = JSON.parse(response.body, symbolize_names: true)
 
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(rb[:data]).to have_key(:id)
+    expect(rb[:data]).to have_key(:type)
+    expect(rb[:data][:type]).to eq("subscription")
+    expect(rb[:data]).to have_key(:attributes)
+    expect(rb[:data][:attributes]).to have_key(:title)
+    expect(rb[:data][:attributes][:title]).to be_a(String)
     
-    require 'pry'; binding.pry
+    expect(rb[:data][:attributes]).to have_key(:frequency)
+    expect(rb[:data][:attributes][:frequency]).to be_a(Integer)
+    expect(rb[:data][:attributes]).to have_key(:price)
+    expect(rb[:data][:attributes][:price]).to be_a(Integer)
+    expect(rb[:data][:attributes]).to have_key(:status)
+    expect(rb[:data][:attributes][:status]).to be(true)
+
+    expect(@customer.subscriptions.count).to eq(2)
   end
 end
